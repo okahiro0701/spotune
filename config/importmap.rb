cc0f4a3dac2f3d61ff7ee2ca97ac1b9583fa745c
okahiro0701 <okanaka19970701@gmail.com>
2024-04-02 12:17:43 +0900

    一覧表示機能のビューの編集

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,5 +3,5 @@
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading"
 pin_all_from "app/javascript/controllers", under: "controllers"
